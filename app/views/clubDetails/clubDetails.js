203d23547281403f2d49b36ecaf31770817b944d
Producenta <producenta@gmail.com>
2016-02-21 18:42:40 +0200

    Added progressbar plugin

--- app/views/clubDetails/clubDetails.js
+++ app/views/clubDetails/clubDetails.js
@@ -9,26 +9,31 @@ var phone = require( "nativescript-phone" );
 function pageNavigatedTo(args) {
   page = args.object;
   let club = args.context;
-  vm = vm.create(club.id, club.name, services);
-  page.bindingContext = vm;
+
+  page.bindingContext = vm.create(club.id, club.name, services);
 }
 
 function facebookTap() {
+	let vm = page.bindingContext;
 	utilityModule.openUrl(vm.facebookUrl);
 }
 
-
 function twitterTap() {
+	 let vm = page.bindingContext;
 	 utilityModule.openUrl(vm.twitterUrl);
 }
 
 function phoneTap() {
+	let vm = page.bindingContext;
 	phone.dial(vm.phone, true);
 }
 
+
 function siteTap() {
+	let vm = page.bindingContext;
 	utilityModule.openUrl(vm.siteUrl);
 }
+
 
 module.exports  = {
 	pageNavigatedTo,
